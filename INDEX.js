5d6764a395277966b30ce0d0e6044f310c609f72
LilPrice-Code <littleprice002@gmail.com>
2019-08-07 15:13:19 -0500

    Completed all problems

---
+++ INDEX.js
@@ -0,0 +1,59 @@
+// Create two variables
+// var anyWord = [ANY NAME]
+// var theSameWord = [ANY NAME]
+// create an if statement comparing the two variables. If they are the same print
+// THAT IS...
+// CORRECT!!!
+//     Else print
+// THAT IS...
+// NOT CORRECT
+
+// var anyWord = prompt("Write a word.");
+// var theSameWord = prompt("Write the same word.");
+//
+// if (anyWord === theSameWord)
+//     {
+//     alert("CORRECT!!!!!!!");
+// }
+// else
+//     {
+//     alert("INCORRECT");
+// }
+
+// Exercise 7
+// // Ask the user for a number. If it’s positive, print “POSITIVE”. If it’s negative, print “NEGATIVE”. If it’s zero, print ZERO.
+
+// var userNum = parseInt(prompt("Give me a number!"));
+//
+// if  (userNum >= 0)
+// {
+//     alert("POSITIVE");
+//
+// }
+// else if (userNum < 0)
+// {
+//     alert("Negative");
+// }
+//
+// //     Exercise 8
+// //
+// // Prints CORRECT if 1, 2, or 3 is entered. Print ERROR for everything else.
+
+var numNum = parseInt(prompt("Give a number."));
+
+if (numNum === 1)
+{
+    alert("Correct");
+}
+else if (numNum === 2)
+{
+    alert("Correct");
+}
+else if (numNum === 3)
+{
+    alert("Correct");
+}
+else
+{
+    alert("Error")
+}
